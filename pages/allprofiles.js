7e6f19fd1210a07956cc07a883dd362867f335ff
kallyas <kallyasmedia@gmail.com>
2021-11-15 15:53:47 +0000

    fix: (FirebaseError): No document to update

--- pages/allprofiles.js
+++ pages/allprofiles.js
@@ -4,7 +4,7 @@ import { collection, getDocs, doc } from "firebase/firestore";
 import Link from "next/link";
 import Image from "next/image";
 
-function Profiles({ data}) {
+function Profiles({ data }) {
   //console.log(data)
   return (
     <section className={styles.right}>
@@ -13,31 +13,31 @@ function Profiles({ data}) {
 
       <div className={styles.projectContent}>
         <div className={styles.projectDetail}>
-         
           {data.map((info) => (
-             <Link href='/profiles/[id]' as={`/profiles/${info.id}`} passHref>
-            <div key={info.id}>
-              <div className={styles.details}>
-                <a target="_blank" rel="noopener noreferrer">
-               
-                  <div className={styles.pic}>
-                  <img src={info.imageUrl} className={styles.pic}   width="100%" height="240" unOptimized/>
-                  </div>
-                  
-                  <p className={styles.title}>
-                 {info.fisrtName} <span>{info.lastName}</span>
-                  </p>
-                  <p className={styles.description}>{info.jobTItle}</p>
-                </a>
+            <Link href="/profiles/[id]" as={`/profiles/${info.id}`} passHref>
+              <div key={info.id}>
+                <div className={styles.details}>
+                  <a target="_blank" rel="noopener noreferrer">
+                    <div className={styles.pic}>
+                      <img
+                        src={info.imageUrl}
+                        className={styles.pic}
+                        width="100%"
+                        height="240"
+                        unptimized="true"
+                      />
+                    </div>
 
+                    <p className={styles.title}>
+                      {info.fisrtName} <span>{info.lastName}</span>
+                    </p>
+                    <p className={styles.description}>{info.jobTItle}</p>
+                  </a>
+                </div>
               </div>
-              
-            </div>
             </Link>
           ))}
-         
         </div>
-        
       </div>
     </section>
   );
@@ -45,7 +45,7 @@ function Profiles({ data}) {
 
 export default Profiles;
 
-export const getServerSideProps  = async () => {
+export const getServerSideProps = async () => {
   let data = [];
   try {
     const projects = await getDocs(collection(db, "profileApplications"));
@@ -58,7 +58,7 @@ export const getServerSideProps  = async () => {
     });
     console.log(data);
   } catch (err) {
-   // console.log(err);
+    // console.log(err);
   }
 
   return {
--- pages/updates/[id].js
+++ pages/updates/[id].js
@@ -19,20 +19,13 @@ function edits({ info }) {
   const [file, setFile] = useState(null);
   const [url, setUrl] = useState(null);
   const [loading, setLoading] = useState(false);
-  const handleChange = (e) =>
-    setUpdate({ ...update, [e.target.name]: e.target.value });
+  const handleChange = (e) => setUpdate({ ...update, [e.target.name]: e.target.value });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const washingtonRef = doc(
-      db,
-      "GDz1y3wBuaXdQkOF9vSt",
-      "profileApplications"
-    ); // Set the "capital" field of the city 'DC'
-
-    await updateDoc(washingtonRef, {
-      ...update,
-    });
+    const profileRef = doc(db, "profileApplications", info.id);
+    await updateDoc(profileRef, update);
+    console.log("updated");
   };
 
   const handleSubmits = async (e) => {
@@ -51,20 +44,10 @@ function edits({ info }) {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-               {" "}
-        <input
-          name="fisrtName"
-          value={update.fisrtName}
-          onChange={handleChange}
-        />
-        <input
-          name="lastName"
-          value={update.lastName}
-          onChange={handleChange}
-        />
-                  <button type="submit">Edit</button>     {" "}
+                <input name="fisrtName" value={update.fisrtName} onChange={handleChange} />
+        <input name="lastName" value={update.lastName} onChange={handleChange} />         {" "}
+        <button type="submit">Edit</button>     {" "}
       </form>
-           {" "}
       {!url && (
         <form onSubmit={handleSubmits}>
           <input
@@ -73,11 +56,7 @@ function edits({ info }) {
             value={update.lmageUrl}
             onChange={(e) => setFile(e.target.files[0])}
           />
-          <button
-            type="submit"
-            className="btn btn-primary mb-2"
-            disabled={loading}
-          >
+          <button type="submit" className="btn btn-primary mb-2" disabled={loading}>
             {loading ? "Uploading..." : "Upload"}
           </button>
           <span>Avatar </span>
@@ -92,10 +71,11 @@ function edits({ info }) {
         </form>
       )}
       {url && (
-        <Link href={url}>    
+        <Link href={url}>
           <a target="_blank">
             <input className={styles.url} type="text" value={url} readOnly />     
-          </a>   
+          </a>
+             
         </Link>
       )}
     </div>
@@ -105,7 +85,7 @@ function edits({ info }) {
 export const getStaticProps = async (context) => {
   const docRef = doc(db, "profileApplications", context.params.id);
   const docSnap = await getDoc(docRef);
-  const info = {id: docSnap.id, ...docSnap.data()};
+  const info = { id: docSnap.id, ...docSnap.data() };
   return {
     props: {
       info,
